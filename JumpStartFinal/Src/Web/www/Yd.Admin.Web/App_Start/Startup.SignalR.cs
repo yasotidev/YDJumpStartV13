a04c2a43aeb71944a386b34c690e1d803fb51b91
yasotidev <yasotidev@outlook.fr>
2014-10-01 23:31:47 +0200

    Add Start up SignalR commit.

---
+++ JumpStartFinal/Src/Web/www/Yd.Admin.Web/App_Start/Startup.SignalR.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Yd.Admin.Web
+{
+    public partial class Startup
+    {
+        public void ConfigureSignalR(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ JumpStartFinal/Src/Web/www/Yd.Admin.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace Yd.Admin.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+
+            ConfigureSignalR(app);
+        }
+    }
+}
