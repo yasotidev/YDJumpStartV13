9d3efc396530a6badecaf2d0c0912ab3f9d2bce6
yasotidev <yasotidev@outlook.fr>
2014-10-01 02:11:53 +0200

    Add Context class comit.

---
+++ JumpStartFinal/Src/Server/Core/Yd.Server.Core.Data/YdAdventureContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Yd.Server.Core.Model;
+
+namespace Yd.Server.Core.Data
+{
+    public class YdAdventureContext : DbContext
+    {
+        public YdAdventureContext():base("YdAdventureEntities")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeePicture> EmployeePictures { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+    }
+}
